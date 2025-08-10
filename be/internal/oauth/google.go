7ba31e3ca15b7d857722b4a502223b23ec71b3ce
dsrice <v.fuji.0223@gmail.com>
2025-08-11 00:14:02 +0900

    feat: Linter修正

--- be/internal/oauth/google.go
+++ be/internal/oauth/google.go
@@ -13,9 +13,9 @@ import (
 type GoogleUserInfo struct {
 	ID            string `json:"id"`
 	Email         string `json:"email"`
-	VerifiedEmail bool   `json:"verified_email"`
 	Name          string `json:"name"`
 	Picture       string `json:"picture"`
+	VerifiedEmail bool   `json:"verified_email"`
 }
 
 type GoogleOAuthService struct{}
